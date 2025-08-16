614ecf813af7a68fe36f7d7027ed427565e1b701
malex167 <malex167@yahoo.com>
2025-08-16 12:48:44 -0600

    Update college placement.R

--- college placement.R
+++ college placement.R
@@ -10,25 +10,27 @@ set.seed(100)
 df <- read.csv("C:/Users/tcaal/Desktop/Data Science Project Stuff/College Placement/College.csv")
 str(df)
 
-df <- df[,-15]
-df$status <- as.factor(df$status)
 
-split <- sample.split(df, SplitRatio = 0.7)
+df <- df[,-15] # Salary variable was removed, salary is directly related to being accepted and doesn't serve as a predictor
+df$status <- as.factor(df$status) # Status converted to factor
+
+split <- sample.split(df, SplitRatio = 0.7) #70:30 split performed on data set
 training <- subset(df, split == TRUE)
 testing <- subset(df, split == FALSE)
 
-RF_model <- randomForest(status ~ ., training)
+RF_model <- randomForest(status ~ ., training) # Random Forest trained with status being dependant on all other variables
 RF_model
-importance(RF_model)
+importance(RF_model) # Displays importance of each variable
 
-prediction <- predict(RF_model, testing)
-confusionMatrix(prediction, testing$status)
-varImpPlot(RF_model)
+prediction <- predict(RF_model, testing) # Predicts values based on the trained model and the testing data set
+confusionMatrix(prediction, testing$status) # Confusion matrix to assess accuracy
+varImpPlot(RF_model) # Plot to visualize variable importance
 
 df2 <- df |>
   mutate(status = case_when(status == "Placed" ~ 1,
-                            status == "Not Placed" ~ 0))
+                            status == "Not Placed" ~ 0)) # New data set for XGboost
 
+# Similar split to prior model along with training and testing sub data sets
 split <- sample.split(df2, SplitRatio = 0.7)
 training <- subset(df2, split == TRUE)
 testing <- subset(df2, split == FALSE)
@@ -50,11 +52,11 @@ xgmodel <- xgb.train(data = xgtrain,
                      max.depth = 3,
                      watchlist = watch,
                      nrounds = 75,
-                     objective = "binary:logistic")
+                     objective = "binary:logistic") # Training XGboost model with status being a binary predicted outcome
 
 
 RMSE <- data.frame(xgmodel$evaluation_log)
-which.min(RMSE$testing_logloss)
+which.min(RMSE$testing_logloss) # minimizing error to avoid over fitting 
 
 XGboost_final <- xgboost(data = xgtrain,
                          max.depth = 3,
@@ -63,9 +65,9 @@ XGboost_final <- xgboost(data = xgtrain,
                          objective = "binary:logistic")
 
 predictionY <- predict(XGboost_final, xgtest)
-predicted_classes <- ifelse(predictionY > 0.5, 1, 0)
+predicted_classes <- ifelse(predictionY > 0.5, 1, 0) # Converting predicted values to dichotomous outcome
 
-confusionMatrix(factor(predicted_classes), factor(testY))
+confusionMatrix(factor(predicted_classes), factor(testY)) # Confusion matrix to assess accuracy
 
 mat <- xgb.importance(feature_names = colnames(trainX),model = xgmodel)
-xgb.plot.importance(importance_matrix = mat)
+xgb.plot.importance(importance_matrix = mat) # Plot to visualize variable contribution
